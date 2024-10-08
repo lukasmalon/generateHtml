f10553646eacae640c2d8066f3009410637bb32e
lukas <malonlukas@gmail.com>
2024-10-08 11:49:17 +0200

    added unescape

--- generateHtml/utils.py
+++ generateHtml/utils.py
@@ -11,12 +11,19 @@ def check_max_occurences(item_list: list, max_occurences: int=1) -> tuple[object
 def prepend_dash_before_uppercase(input_str: str) -> str:
     return re.sub(r'([A-Z])', r'-\1', input_str)
 
-def escape_html(text:str):
+def escape_html(text:str) -> str:
     return text.replace('&', '&amp;')\
                 .replace('>', '&gt;')\
                 .replace('<', '&lt;')\
                 .replace('\'','&#39;')\
                 .replace('"','&#34;')
+
+def unescape_html(text: str) -> str:
+    return text.replace('&#34;', '"')\
+               .replace('&#39;', "'")\
+               .replace('&lt;', '<')\
+               .replace('&gt;', '>')\
+               .replace('&amp;', '&')
 
 def get_class_from_string(module_name: str, class_name: str) -> callable:
     module = importlib.import_module(module_name)
